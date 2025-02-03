5c54659447b62abac35243e42bdf10f0a73a66d1
yaswanth sandra <yaswanthsandra143@gmail.com>
2025-02-03 10:40:29 +0530

    Create Arthimetic.java

---
+++ Arthimetic.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Arthimetic {
+	public static void main(String[] args) {
+	int a = 15;
+	int b = 15;
+	System.out.println("Addition = "  + (a + b));
+	System.out.println("Subraction = " + (a -b));
+	System.out.println("Multilication = "  + (a * b));
+	System.out.println("Division = " + (a / b));
+	System.out.println("Modulus = " + (a % b));
+}
+}
